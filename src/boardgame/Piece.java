f2a79a0dde070d2e9787737843818fc7cf0665fb
Victor-Travaglia <victortravaglia8@gmail.com>
2019-05-06 13:54:32 -0300

    Implementando movimentos possíveis da torre

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -29,7 +29,6 @@ public abstract class Piece {
 				}
 			}
 		}
-
 		return false;
 	}
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPostion.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
@@ -48,6 +49,12 @@ public class ChessMatch {
 
 		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw  new ChessException("Não há movimento possível para a peça escolhida.");
+		}
+	}
+
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("A peça escolhida não pode se mover para a posição de destino.");
 		}
 	}
 
